2823e37356bac7f1b4c946657c1e06b23c86b7b9
Sree1710 <kunjusree817@gmail.com>
2022-07-20 23:48:09 +0530

    patient homepage created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DoctorhomepageComponent } from './doctorhomepage/doctorhomepage.component';
+import { PatienthomepageComponent } from './patienthomepage/patienthomepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    DoctorhomepageComponent
+    DoctorhomepageComponent,
+    PatienthomepageComponent
   ],
   imports: [
     BrowserModule,
